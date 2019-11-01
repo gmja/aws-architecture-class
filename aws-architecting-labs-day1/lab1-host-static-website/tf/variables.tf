5ac960e0f5108a8e86f841d1b4a8e5b3db06db61
rfrancisco <rfrancisco@salesforce.com>
2019-11-01 15:32:05 -0400

    Removed AWS keys

--- aws-architecting-labs-day1/lab1-host-static-website/tf/variables.tf
+++ aws-architecting-labs-day1/lab1-host-static-website/tf/variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "aws_access_key" {
-  description = "Access key to use with AWS provider"
-  default     = "AKIA2VFJSCGXHWMPW7AT"
-}
-
-variable "aws_secret_key" {
-  description = "Secret key to use with AWS provider"
-  default     = "aw4JH2ko60o8tDsVg/n8BLuh54DMvBhm4s4l2uL7"
-}
-
 variable "web_bucket_name" {
   description = "The name of the web bucket to create"
   default     = "francisco-rod-aws-arch-labone-web"
--- aws-architecting-labs-day1/lab2-vpc-and-web-app/tf/variables.tf
+++ aws-architecting-labs-day1/lab2-vpc-and-web-app/tf/variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "aws_access_key" {
-  description = "Access key to use with AWS provider"
-  default     = "AKIA2VFJSCGXHWMPW7AT"
-}
-
-variable "aws_secret_key" {
-  description = "Secret key to use with AWS provider"
-  default     = "aw4JH2ko60o8tDsVg/n8BLuh54DMvBhm4s4l2uL7"
-}
-
 variable "vpc_name" {
   description = "The name of the VPC to create"
   default     = "rod-francisco-vpc"
